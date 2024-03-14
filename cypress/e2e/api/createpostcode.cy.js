0f3968159ee8d659ede0023d7d35352bfd3975dd
Anthony McGlone <102866938+anthonymcglone2022@users.noreply.github.com>
2024-03-14 11:14:08 +0000

    Update createpostcode.cy.js

--- cypress/e2e/api/createpostcode.cy.js
+++ cypress/e2e/api/createpostcode.cy.js
@@ -5,21 +5,21 @@ describe('API Automation Suite', () => {
    */
 
   it('Create Post Code', () => {
-    cy.fixture('postcode').then((data)=>{
-        const request_body=data;
+      cy.fixture('postcode').then((data)=>{
+          const request_body=data;
 
-        cy.request({
-            method: 'POST',
-            url: 'http://127.0.0.1:9191/createPostCode',
-            body:request_body
-        })
-        .then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response.body.code).to.eq(request_body.code)
-            expect(response.body.inuse).to.eq(request_body.inuse)
-            expect(response.body.district).to.eq(request_body.district)
-        })
-    })
+          cy.request({
+              method: 'POST',
+              url: 'http://127.0.0.1:9191/createPostCode',
+              body:request_body
+          })
+          .then((response) => {
+              expect(response.status).to.eq(200)
+              expect(response.body.code).to.eq(request_body.code)
+              expect(response.body.inuse).to.eq(request_body.inuse)
+              expect(response.body.district).to.eq(request_body.district)
+          })
+      })
   })
 
 })
